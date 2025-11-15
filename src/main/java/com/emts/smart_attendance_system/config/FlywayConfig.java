96f9085654e08d87597eeb65cf85ec263d670b94
mohamed742590taha-wq <mohamed742590taha@gmail.com>
2025-11-16 01:26:57 +0200

    Initial commit - eMTS Smart Attendance System

--- src/main/java/com/emts/smart_attendance_system/config/FlywayConfig.java
+++ src/main/java/com/emts/smart_attendance_system/config/FlywayConfig.java
@@ -1,10 +1,5 @@
 package com.emts.smart_attendance_system.config;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,34 +7,26 @@ import org.springframework.context.annotation.Configuration;
  * File: FlywayConfig.java
  * Package: com.emts.smart_attendance_system.config
  * Project: eMTS Smart Attendance System
- * © ٢٠٢٥ Mohamed Taha Elmeligy - eMTS (e Modern Tech Solutions)
+ * © 2025 Mohamed Taha Elmeligy - eMTS (e Modern Tech Solutions)
  * This file is part of the eMTS Smart Attendance System.
  * Created on: 26/10/2025
  * Port Number: 8083
  * *******************************************************************
+
+ * Flyway Configuration
+
+ * Spring Boot Auto Configuration يتولى Flyway تلقائياً
+ * يقرأ الإعدادات من application.properties:
+ *   spring.flyway.enabled=true
+ *   spring.flyway.url=${FLYWAY_URL:...}
+ *   spring.flyway.user=${DATABASE_USER:...}
+ *   spring.flyway.password=${DATABASE_PASSWORD:...}
+ *   spring.flyway.locations=classpath:db/migration/common,classpath:db/migration/postgre
+ *   spring.flyway.baseline-on-migrate=true
+ *   spring.flyway.validate-on-migrate=true
  */
-
 @Configuration
-@EnableConfigurationProperties(FlywayProperties.class)
 public class FlywayConfig {
-
-    @Value("${spring.flyway.url}")
-    private String flywayUrl;
-
-    @Value("${spring.flyway.user}")
-    private String flywayUser;
-
-    @Value("${spring.flyway.password}")
-    private String flywayPassword;
-
-    @Bean(initMethod = "migrate")
-    public Flyway flyway() {
-        return Flyway.configure()
-                .dataSource(flywayUrl, flywayUser, flywayPassword)
-                .locations("classpath:db/migration/common", "classpath:db/migration/postgre")
-                .baselineOnMigrate(true)
-                .baselineVersion("0")
-                .validateOnMigrate(true)
-                .load();
-    }
+    // Spring Boot Auto Configuration يتولى كل شيء
+    // لا تحتاج تعريف Bean يدويًا
 }
