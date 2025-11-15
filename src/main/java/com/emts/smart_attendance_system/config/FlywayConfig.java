cdf143a09b2ec25d3226a51544efc0d6c5a28d61
mohamed742590taha-wq <mohamed742590taha@gmail.com>
2025-11-15 04:10:24 +0200

    Initial commit - eMTS Smart Attendance System

--- src/main/java/com/emts/smart_attendance_system/config/FlywayConfig.java
+++ src/main/java/com/emts/smart_attendance_system/config/FlywayConfig.java
@@ -1,6 +1,7 @@
 package com.emts.smart_attendance_system.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,29 +23,23 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(FlywayProperties.class)
 public class FlywayConfig {
 
-    /**
-     * يقوم بإنشاء وتكوين Flyway Bean باستخدام بيانات من application.properties
-     * يقرأ التالي من الـ Properties:
-     * - spring.flyway.url
-     * - spring.flyway.user
-     * - spring.flyway.password
-     * - spring.flyway.locations
-     * - spring.flyway.baseline-on-migrate
-     * - spring.flyway.baseline-version
-     * - spring.flyway.clean-disabled
-     */
+    @Value("${spring.flyway.url}")
+    private String flywayUrl;
+
+    @Value("${spring.flyway.user}")
+    private String flywayUser;
+
+    @Value("${spring.flyway.password}")
+    private String flywayPassword;
+
     @Bean(initMethod = "migrate")
-    public Flyway flyway(FlywayProperties flywayProperties) {
+    public Flyway flyway() {
         return Flyway.configure()
-                .dataSource(
-                        flywayProperties.getUrl(),
-                        flywayProperties.getUser(),
-                        flywayProperties.getPassword()
-                )
-                .locations(flywayProperties.getLocations().toArray(new String[0]))
-                .baselineOnMigrate(flywayProperties.isBaselineOnMigrate())
-                .baselineVersion(flywayProperties.getBaselineVersion())
-                .cleanDisabled(flywayProperties.isCleanDisabled())
+                .dataSource(flywayUrl, flywayUser, flywayPassword)
+                .locations("classpath:db/migration/common", "classpath:db/migration/postgre")
+                .baselineOnMigrate(true)
+                .baselineVersion("0")
+                .validateOnMigrate(true)
                 .load();
     }
 }
